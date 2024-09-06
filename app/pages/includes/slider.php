113f65889bdedb8dce33e7b642b8d49e20518a66
ArkarAyeMyint <danielarkar36@gmail.com>
2024-09-06 19:43:12 +0630

    healthy body final outcome

---
+++ app/pages/includes/slider.php
@@ -0,0 +1,22 @@
+
+  <!-- slider -->
+  <link rel="stylesheet" href="<?=ROOT?>/assets/slider/ism/css/my-slider.css"/>
+  <script src="<?=ROOT?>/assets/slider/ism/js/ism-2.2.min.js"></script>
+
+  <div class="ism-slider" data-transition_type="fade" data-play_type="loop" id="my-slider">
+    <ol>
+      <li>
+        <img src="<?=ROOT?>/assets/slider/ism/image/slides/1.jpg">
+      </li>
+      <li>
+        <img src="<?=ROOT?>/assets/slider/ism/image/slides/2.jpg">
+      </li>
+      <li>
+        <img src="<?=ROOT?>/assets/slider/ism/image/slides/3.jpg">
+      </li>
+      <li>
+        <img src="<?=ROOT?>/assets/slider/ism/image/slides/4.jpg">
+      </li>
+    </ol>
+  </div>
+  <!-- end slider -->
